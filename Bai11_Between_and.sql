d7ca22cac6f9c6d0ee7efb2df85dbcf01f79bafa
Dao Tien Dung <daotiendung0607@gmai.com>
2023-12-19 17:02:14 +0700

    Add Bai11 va Bai12

---
+++ Bai11_Between_and.sql
@@ -0,0 +1,29 @@
+﻿--Lay ra danh sach cac san pham co gia ban tu 10 den 20$
+SELECT *
+FROM [dbo].[Products]
+WHERE [UnitPrice] BETWEEN 10 AND 20;
+
+--Lay ra danh sach cac don dat hang tu 1996-07-01 den 1996-07-31
+SELECT *
+FROM [dbo].[Orders]
+WHERE [OrderDate] BETWEEN '1996-07-01' AND '1996-07-31';
+
+--Tinh tong so tien van chuyen(Freight) cua cac don hang dat trong thang 7 nam 1996
+SELECT SUM(Freight) AS "TotalJullyFreight"
+FROM [dbo].[Orders]
+WHERE [OrderDate] BETWEEN '1996-07-01' AND '1996-07-31';
+
+--BT1: Lay danh sach cac don dat hang trong nam 1997 va duoc van chuyen bang duong thuy (ShipVia=3)
+SELECT *
+FROM [dbo].[Orders]
+WHERE (ShipVia = 3) AND ([OrderDate] BETWEEN '1997-01-01' AND '1997-12-31');
+
+--BT2.Tính  giá van chuyen trung  bình  của  các  sản  phẩm có  ngày  đặt  hàng trong khoảng từ ngày  1/1/1996 đến  ngày  31/7/1997
+SELECT AVG([Freight]) AS "AVGFreight"
+FROM [dbo].[Orders]
+WHERE [OrderDate] BETWEEN '1996-01-01' AND '1997-07-31';
+
+--BT3: Liệt  kê  tất  cả  các  sản phẩm có đơn giá trong  khoảng từ 50 đến  100 và  Category=1
+SELECT *
+FROM [dbo].[Products]
+WHERE ([UnitPrice] BETWEEN 50 AND 100) AND ([CategoryID]=1);
---
+++ Bai12_Tu_khoa_Like.sql
@@ -0,0 +1,35 @@
+﻿--Loc ra tat ca cac khach hang, cac khach hang nay den tu quoc gia co bat dau bang chu 'A'
+SELECT *
+FROM [dbo].[Customers]
+WHERE [Country] LIKE 'A%';
+
+--Lay ra tat ca cac don dat hang, duoc gui den thanh pho co chua chu 'a'
+SELECT *
+FROM [dbo].[Orders]
+WHERE [ShipCity] LIKE '%a%';
+
+--Loc ra tat ca cac don hang voi 
+-- ShipCountry LIKE 'U_'
+-- ShipCountry LIKE 'U%'
+SELECT *
+FROM [dbo].[Orders]
+WHERE [ShipCountry] LIKE 'U_';
+
+SELECT *
+FROM [dbo].[Orders]
+WHERE [ShipCountry] LIKE 'U%';
+
+--BT1: Lay ra tat ca cac nha cung cap co chu 'b' trong ten Cong ty cua ho
+SELECT *
+FROM [dbo].Suppliers
+WHERE [CompanyName] LIKE '%b%'
+
+--BT2: Hãy lấy ra tất cả các Employee có ký tự thứ 3 là 's' trong TitleOfCourtesy
+SELECT *
+FROM [dbo].[Employees]
+WHERE [TitleOfCourtesy] LIKE '__s%';
+
+--BT3: Liệt kệ danh sách các Suppliers có số điện thoại :có chứa số 4 và có đuôi 22 (Bảng Suppliers).
+SELECT *
+FROM [dbo].[Suppliers]
+WHERE [Phone] LIKE '%4%22';
